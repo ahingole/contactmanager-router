5358f2be020ee2a719ae790a19a55f214ed12dbc
akash <ahingolewsm@gmail.com>
2022-05-15 11:36:59 +0530

    c

--- src/component/pro.js
+++ src/component/pro.js
@@ -1,23 +1,8 @@
 import React from 'react'
 
-<<<<<<< HEAD
 export default function pro() {
   return (
     <div>pro this os practice for git commit </div>
 
   )
 }
-=======
- function Pro() {
-  return (
-    <div>pro
-
-        <form>
-            <input type="text"/> 
-        </form>
-    </div>
-  )
-}
-
-export default Pro
->>>>>>> probranch
