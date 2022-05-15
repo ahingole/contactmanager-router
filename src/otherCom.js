8940149bfd45923e888d6b647a7f1f3c384c00e3
akash <ahingolewsm@gmail.com>
2022-05-15 09:40:43 +0530

    changes for mergr

--- src/otherCom.js
+++ src/otherCom.js
@@ -2,13 +2,6 @@ import React from 'react'
 
 export default function otherCom() {
   return (
-<<<<<<< HEAD
-    <div>
-    <h1>to create conflict </h1>
-
-    </div>
-=======
     <div>otherCom making changes in repo</div>
->>>>>>> ec66509f099edf152b0ed522e83e14d4feade4de
   )
 }
