9d14ade756c462ca504652c6418971e718565d67
akash <ahingolewsm@gmail.com>
2022-05-15 10:42:27 +0530

    this changes for pro component

---
+++ src/component/pro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function Pro() {
+  return (
+    <div>pro
+
+        <form>
+            <input type="text"/> 
+        </form>
+    </div>
+  )
+}
+
+export default Pro
